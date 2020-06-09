ac68a51d4ddc2ee612b7eac6d229c60fb9d28a6f
ju la <>
2020-06-09 17:50:06 +0200

    second commit

---
+++ src/main/java/com/example/demo/DemoXsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoXsdApplication.class, args);
+	}
+
+}
